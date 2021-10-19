13a7e6af42ad35c63ef17157c77899d368bfbf28
LAKROUZ Amirouche <PINF67@bm-energies.com>
2021-10-19 10:34:29 +0200

    init spring boot project

---
+++ atos-usermanager/src/main/java/com/usermanager/atosusermanager/AtosUsermanagerApplication.java
@@ -0,0 +1,13 @@
+package com.usermanager.atosusermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtosUsermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtosUsermanagerApplication.class, args);
+	}
+
+}
